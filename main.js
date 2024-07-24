be2d3651ae799ec3bf3c31dbbcb61c580d93459c
Ana-Maria Draganuta <anamariadraganuta95@gmail.com>
2024-07-25 00:40:49 +0300

    Change function

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const result = document.getElementById("result");
+let resultDisplayed = false;
 
 function getResult(value) {
   if (value === "=") {
-    result.value = eval(result.value)
+    const expression = result.value;
+    if (expression.includes("/0")) {
+      result.value = "Eroare";
+    } else {
+      result.value = eval(expression);
+    }
+    resultDisplayed = true;
   } else {
+    if (resultDisplayed) {
+      result.value = "";
+      resultDisplayed = false;
+    }
     result.value += value;
   }
 }
@@ -11,3 +22,12 @@ function getResult(value) {
 function clearResult() {
   result.value = "";
 }
+
+function deleteLast() {
+    if (resultDisplayed) {
+      result.value = "";
+      resultDisplayed = false;
+    } else {
+      result.value = result.value.slice(0, -1);
+    }
+  }
